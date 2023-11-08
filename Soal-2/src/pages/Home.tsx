388da9fbb49d50a8e0264a1ba6f3740b8b992065
rafiakbar13 <muhammadrafia13@gmail.com>
2023-11-08 18:58:06 +0700

    added section companies and about

--- Soal-2/src/pages/Home.tsx
+++ Soal-2/src/pages/Home.tsx
@@ -1,12 +1,18 @@
+import About from '../components/About'
+import Companies from '../components/Companies'
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
 type Props = {}
 
 const Home = (props: Props) => {
     return (
-        <div>
-            <Navbar />
-            <Hero />
+        <div className='w-full mx-auto max-w-7xl'>
+            <main>
+                <Navbar />
+                <Hero />
+                <Companies />
+                <About />
+            </main>
         </div>
     )
 }
